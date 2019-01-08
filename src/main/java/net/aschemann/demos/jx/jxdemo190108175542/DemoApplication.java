da08d3096c2c9a4cadd649c2b7d5f17f7b530568
Gerd Aschemann <gerd@aschemann.net>
2019-01-08 17:56:40 +0100

    Initial import

---
+++ src/main/java/net/aschemann/demos/jx/jxdemo190108175542/DemoApplication.java
@@ -0,0 +1,14 @@
+package net.aschemann.demos.jx.jxdemo190108175542;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
